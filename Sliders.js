6a073bb6382361f4a7e0253e16f883331257d76a
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 17:01:40 -0400

    the js update
    maybe it looks better?

--- Sliders.js
+++ Sliders.js
@@ -4,13 +4,29 @@ function fixVerticalCursor(ems) {
     $("body").append(div);
 }
 function commandNameSlider(name) {
-    var img = $("<img>"), txt = $("<h1>");
+    var div = $("<div>"), img = $("<img>"), txt = $("<h1>");
     img.attr("src", "CommandSlider.png");
     img.attr("style", "width:30em; height:4em;");
     txt.text(name);
-    txt.attr("style", "position:relative;bottom:1.25em;right:-1em;");
-    $("body").append(img);
-    $("body").append(txt);
+    txt.attr("style", "position:relative;bottom:1.3em;right:-1em;margin-left:0.5em;margin-right:10em;");
+    div.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:500px;")
+    div.append(img);
+    div.append(txt);
+    $("body").append(div);
+    fixVerticalCursor("2.5em");
+}
+
+function adminCommandNameSlider(name) {
+    var div = $("<div>"), img = $("<img>"), txt = $("<h1>");
+    img.attr("src", "CommandSlider.png");
+    img.attr("style", "width:30em; height:4em;");
+    txt.text(name);
+    txt.attr("style", "color:red;position:relative;bottom:1.3em;right:-1em;margin-left:0.5em;margin-right:10em;");
+    div.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:500px;")
+    div.append(img);
+    div.append(txt);
+    $("body").append(div);
+    fixVerticalCursor("2.5em");
 }
 
 function subcommandCard(name, syntax, note) {
@@ -24,15 +40,38 @@ function subcommandCard(name, syntax, note) {
     body.append(nte);
 
     img.attr("src","SubcommandNameSlider.png");
-    img.attr("style", "width:22em;margin-top:1.5em;")
+    img.attr("style", "width:25em;margin-top:1.5em;")
     txt.html(name);
-    txt.attr("style", "position:relative;bottom:1em;right:-2.5em;font-size:30pt;margin-right:6em");
+    txt.attr("style", "position:relative;bottom:1.2em;right:-2.5em;margin-right:9em;margin-left:0.5em;");
     subcontainer.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:300px;");
     subcontainer.append(img);
     subcontainer.append(txt);
     container.append(subcontainer);
     container.append(body);
     $("body").append(container);
+    fixVerticalCursor("1.5em");
+}
+function adminsubcommandCard(name, syntax, note) {
+    var container = $("<div>"), subcontainer = $("<div>"), img = $("<img>"), txt = $("<h1>"), body = $("<p>"), syn = $("<span>"), nte = $("<p>");
+    body.attr("style", "margin-top:-3em;margin-left:75px");
+    syn.addClass("iabCommandSyntax");
+    syn.html(syntax);
+    nte.html(note);
+    body.append(syn);
+    body.append("<br/>");
+    body.append(nte);
+
+    img.attr("src","SubcommandNameSlider.png");
+    img.attr("style", "width:25em;margin-top:1.5em;")
+    txt.html(name);
+    txt.attr("style", "color:red;position:relative;bottom:1.2em;right:-2.5em;margin-right:9em;margin-left:0.5em;");
+    subcontainer.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:300px;");
+    subcontainer.append(img);
+    subcontainer.append(txt);
+    container.append(subcontainer);
+    container.append(body);
+    $("body").append(container);
+    fixVerticalCursor("1.5em");
 }
 
 function br() {
@@ -56,7 +95,33 @@ function SubcommandsSlider() {
     div.append(img);
     div.append(txt);
     $("body").append(div);
-
+    fixVerticalCursor("2.5em");
+}
+function UnorderedParametersSlider() {
+    var img = $("<img>"), txt = $("<h1>");
+    var div = $("<div>");
+    div.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:300px;");
+    img.attr("src", "SubcommandSlider.png");
+    img.attr("style", "width:25em; height:4em; margin-bottom: 0px");
+    txt.text("Unordered Parameters");
+    txt.attr("style", "position:relative;bottom:2.35em;right:-2.5em;line-height:100%;font-size:200%;margin-right:3em;");
+    div.append(img);
+    div.append(txt);
+    $("body").append(div);
+    fixVerticalCursor("6.5em");
+}
+function OrderedParametersSlider() {
+    var img = $("<img>"), txt = $("<h1>");
+    var div = $("<div>");
+    div.attr("style", "text-align:center;vertical-align:middle;line-height:1em;width:300px;");
+    img.attr("src", "SubcommandSlider.png");
+    img.attr("style", "width:25em; height:4em; margin-bottom: 0px");
+    txt.text("Ordered Parameters");
+    txt.attr("style", "position:relative;bottom:2.35em;right:-2.5em;line-height:100%;font-size:200%;margin-right:3em;");
+    div.append(img);
+    div.append(txt);
+    $("body").append(div);
+    fixVerticalCursor("6.5em");
 }
 
  function indexWarpSlider() {
@@ -66,5 +131,6 @@ function SubcommandsSlider() {
     img.attr("style", "height:2em;margin-bottom: 0px");
     a.append(img);
     $("body").append(a);
+    br();
 
 }
